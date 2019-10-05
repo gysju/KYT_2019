608ba3c1f28ee95531de0aa261808dddfe5a5638
gysju <gysenjulien@gmail.com>
2019-10-05 03:07:44 +0200

    genrete map and player movement

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public static MapManager Instance;
+
+    [SerializeField] private int m_SizeX = 10;
+    [SerializeField] private int m_SizeY = 10;
+
+    [SerializeField] private float m_TileSizeX = 1.0f;
+    [SerializeField] private float m_TileSizeY = 1.0f;
+
+    public Vector3[][] m_MapCoordonate;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            IniteMap();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public int GetMapSizeX()
+    {
+        return m_SizeX;
+    }
+
+    public int GetMapSizeY()
+    {
+        return m_SizeY;
+    }
+
+    private void IniteMap()
+    {
+        m_MapCoordonate = new Vector3[m_SizeX][];
+        for (int i = 0; i < m_MapCoordonate.Length; i++)
+        {
+            m_MapCoordonate[i] = new Vector3[m_SizeY];
+            for (int j = 0; j < m_MapCoordonate[i].Length; j++)
+            {
+                m_MapCoordonate[i][j] = new Vector3( (i * m_TileSizeX - ((m_SizeX - 1) * m_TileSizeX * 0.5f )),
+                                                     (j * m_TileSizeY - ((m_SizeY - 1) * m_TileSizeY * 0.5f)), 
+                                                     0.0f);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_MapCoordonate == null)
+            return;
+
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < m_MapCoordonate.Length; i++)
+        {
+            for (int j = 0; j < m_MapCoordonate[i].Length; j++)
+            {
+                Gizmos.DrawWireSphere(m_MapCoordonate[i][j], 1.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int m_ID;
+
+    private int m_Xpos;
+    private int m_Ypos;
+
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        m_Xpos = 0;
+        m_Ypos = 0;
+    }
+
+    void Update()
+    {
+        Movement();    
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_Ypos = Mathf.Clamp(m_Ypos - 1, 0, MapManager.Instance.GetMapSizeY() - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            m_Ypos = Mathf.Clamp(m_Ypos + 1, 0, MapManager.Instance.GetMapSizeY() - 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            m_Xpos = Mathf.Clamp(m_Xpos - 1, 0, MapManager.Instance.GetMapSizeX() - 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_Xpos = Mathf.Clamp(m_Xpos + 1, 0, MapManager.Instance.GetMapSizeX() - 1);
+        }
+
+        transform.position = MapManager.Instance.m_MapCoordonate[m_Xpos][m_Ypos];
+    }
+}
